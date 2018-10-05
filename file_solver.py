32d2e9c9f82395cd2544e61f667548e26d38e272
abdo <ar1813@fayoum.edu.eg>
2018-10-05 21:31:40 +0200

    update doc for fun extract problems

--- file_solver.py
+++ file_solver.py
@@ -1,11 +1,13 @@
 import os
 
 def calc(num1 , operation , num2):
-    '''calc function document
+    '''
     this is function do basic calculation (+  -  *  /  ^  %) betweewn two numbers with operation passed as string
+    
     #parameters 
     num1 ,num: numbers to be calculated
-    operation: a string represent the operation bewean the two numbers
+    operation: a string represent the operation betwean the two numbers
+    
     #examples
     >>>print (calc(4,'*',-5))
     -20 
@@ -26,23 +28,46 @@ def calc(num1 , operation , num2):
         return num1%num2
     else:
         print (num1 + operation + num2 +"not valid operation")
-        #TODO print valid operations
+        print("valid operations are (+  -  *  /  ^  %)")
 
-def extract_problems():    
+def extract_problems(file_name = "problems.txt"):    
+    '''
+    this function reads problem from a text file and return them as a list 
+    of seprated problems as string
+    if no file included it will create a new one
+    called problem.text in same directory
+
+    # parametars
+    file_name : the path of the file or just name if same directory
+
+    # problem.txt sample
+    1- 0
+    1 +      2
+    -10 * 3
+
+    # example usage
+    >>> print (extract_problems("problem.txt"))
+    ['1- 0','1 +   2','-10 * 3']
+    '''
     #try opening file if no file excest create it
     try:
-        problem_file=open("problems.txt","rt")
+        problem_file=open(file_name,"rt")
     except IOError:
-        problem_file = open("problems.txt","a")
+        problem_file = open(file_name,"a")
         problem_file.close()
-        print ("please add problems to problem.txt")
+        print ("please add problems to "+file_name)
         exit()
+
     #extracting problems from file exit if nothing excest
     problems = problem_file.read()
     problems = problems.split('\n')
+    
+    #if file is empty close
     if len(problems) == 1 and len(problems[0]) == 0:
         print ("please add problems to problem.txt \nfile is empty")        
         exit()
+
+    #close file and return problems
     else:
         problem_file.close()
         return problems
